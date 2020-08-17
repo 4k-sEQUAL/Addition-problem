2d18398a760001a0e8fee1da48c63d7cc1397817
4ksEQUAL <nickteague23@gmail.com>
2020-08-17 22:12:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+num1 = int(input ("Pick a Number "))
+if num1 > 0:
+  print ("That number is positive")
+elif num1 < 0:
+  print ("That number is negative")
+elif num1 == 0:
+  print ("That number is 0, which is neither negative nor positive")
